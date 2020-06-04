a90721997987eff004007c93110a58f864ff0c1e
Gagandeep Singh <gagandp406@gmail.com>
2020-06-05 03:40:23 +0530

    Test comment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-// console.log(__dirname)
+// console.log(__dirname) 
+//Testing
 // console.log(path.join(__dirname, '../public'))
 // console.log(__filename)
 
